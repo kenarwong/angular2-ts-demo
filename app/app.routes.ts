0cb0c53f83900e771df4289d6a82b27bd09920cf
kennyhwang <ken.r.hwang@gmail.com>
2016-08-03 12:03:04 -0400

    router paramter, router links/navigate, finished css/html relative files

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,8 +1,13 @@
 import { provideRouter, RouterConfig }      from '@angular/router';
 import { HeroesComponent }                  from './heroes.component';
 import { DashboardComponent }                from './dashboard.component';
+import { HeroDetailComponent }                  from './hero-detail.component';
 
 const routes: RouterConfig = [
+    {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+    },
     {
         path: 'heroes',
         component: HeroesComponent
