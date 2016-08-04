18200d70b81f8efe84bdc28ae98352f94df4fccd
kennyhwang <ken.r.hwang@gmail.com>
2016-08-04 14:15:06 -0400

    setting up backend mock service, unstable

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HEROES } from './mock-heroes';
+import { Http } from '@angular/http';
+//import { HEROES } from './mock-heroes.obsolete';
+import 'rxjs/add/operator/toPromise';
+import { Hero } from './hero';
 
 @Injectable()
 export class HeroService {
-    getHeroes() {
-        return Promise.resolve(HEROES);
+    private heroesUrl = 'app/heroes';  // URL to web api
+
+    constructor(private http: Http) { }
+
+    private handleError(error: any) {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
     }
+
+    getHeroes() {
+        return this.http.get(this.heroesUrl)
+        .toPromise()
+        .then(response => response.json().data as Hero[])
+        .catch(this.handleError);
+    }
+
     getHero(id: number) {
         return this.getHeroes()
             .then(heroes => heroes.find(hero => hero.id === id));
